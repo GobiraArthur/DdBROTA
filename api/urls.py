120a66bd2bb368ab66aa3fe66a04684f02284102
Leane Soares <leanesoares212@gmail.com>
2024-08-14 19:48:54 -0300

    CRUD necessidade de manutenção

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from api.views import telefone_emergencial_view
+from api.views import necessidades_de_manutencao_views
 
 urlpatterns = [
     # Telefone Emergencial
@@ -10,4 +11,12 @@ urlpatterns = [
     path('telefone/atualizar/<int:id>/', telefone_emergencial_view.atualizar),
     path('telefone/remover/<int:id>/', telefone_emergencial_view.remover),
     path('telefone/remover/', telefone_emergencial_view.remover_lista),
+
+    # Necessidade de Manutenção 
+    path('necessidade/', necessidades_de_manutencao_views.listar),
+    path('necessidade/<int:id>/', necessidades_de_manutencao_views.buscar_por_id),
+    path('necessidade/criar/', necessidades_de_manutencao_views.criar),
+    path('necessidade/atualizar/<int:id>/', necessidades_de_manutencao_views.atualizar),
+    path('necessidade/remover/<int:id>/', necessidades_de_manutencao_views.deletar), 
+
 ]
---
+++ core/model/necessidades_de_manutencao.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class NecessidadeDeManutencao(models.Model):
+    
+        id = models.AutoField(primary_key=True)
+        nome = models.CharField(max_length=50, blank=False)
+        criado_em = models.DateTimeField(auto_now_add=True)
+        atualizado_em = models.DateTimeField(auto_now=True)  
+        deletado_em = models.DateTimeField(null=True)   
+    
+        def __str__(self):
+            return f'{self.id} - {self.nome}'
+        
